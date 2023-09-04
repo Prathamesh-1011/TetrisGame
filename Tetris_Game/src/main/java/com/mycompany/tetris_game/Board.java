7f4be0a991c42febb599302706101d346296f2df
Prathamesh-1011 <144042848+Prathamesh-1011@users.noreply.github.com>
2023-09-04 20:58:18 +0530

    Update Board.java

--- Tetris_Game/src/main/java/com/mycompany/tetris_game/Board.java
+++ Tetris_Game/src/main/java/com/mycompany/tetris_game/Board.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.tetris_game;
 
-/**
- *
- * @author Pranay Rokade
- */
 
 import java.awt.Color;
 import java.awt.Graphics;
